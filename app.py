e732daa8605a41a4324ad50db88f37937894582f
dLCamm <camilodeleon229@gmail.com>
2025-11-11 15:54:04 -0600

    feat(doctores.html): Modulo de Doctores Agregado

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def inicio():
 @app.route("/servicios")
 def servicios():
     usuarios = [
-        [1, "Juan Perez", "Perez", 1999, 2000],
+        [1, "Revisión General", "Evaluación de dolores musculares", 100, 80],
         [2, "Maria Gomez", "Gomez", 2001, 2002],
         [3, "Carlos Ruiz", "Ruiz", 1998, 1999]]
     return render_template("servicios.html", usuarios=usuarios)
@@ -17,20 +17,20 @@ def servicios():
 @app.route("/lista_citas")
 def lista_citas():
     usuarios = [
-        [1, "Juan Perez", 1999, "Samuel", "revision", "aceptad"],
-        [2, "Maria Gomez", 2001, "Samuel", "pan", "terminada"],
-        [3, "Carlos Ruiz", 1998, "Samuel", "lingo", "en proceso"]]
+        [1, "Brandom", "2025-11-02", "Revision General", "Alergico a la penicilina", 50, "Juan Perez"],
+        [2, "Maria Gomez", 2001, "Samuel", "pan",  60, "Maria Gomez"],
+        [3, "Carlos Ruiz", 1998, "Samuel", "lingo", 40, "Carlos Ruiz"]]
     return render_template("lista_citas.html", usuarios=usuarios)
 
 
 @app.route("/editar_servicios/<int:id>")
 def editar_servicios(id):
-    usuario = [1, "Juan Perez", "Perez", 1999, 2000, "12894248", "Xela"]
+    usuario = [1, "Revisión General", "Evaluación de dolores dentales", 100, 80]
     return render_template("editar_servicios.html", usuario=usuario)
 
 @app.route("/editar_citas/<int:id>")
 def editar_cita(id):
-    usuario = [1, "Juan Perez", "Perez", 1999, 2000, "12894248"]
+    usuario = [1, "Juan Perez", "Evaluación de dolores dentales", 1999, 2000, "12894248", "Dr. Carlos Ruiz"]
     return render_template("editar_citas.html", usuario=usuario)
 
 @app.route("/registro_serv")
@@ -44,6 +44,23 @@ def registrocita():
 @app.route("/eliminar_usuario")
 def eliminar_usuario():
     return "Usuario eliminado"
+
+@app.route("/doctores")
+def doctores():
+    return render_template("doctores.html", usuarios=[
+        [1, "Dr. Juan Perez", "Cardiologia", "05/10/2020", "555-1234"],
+        [2, "Dra. Maria Gomez", "Neurologia", "2018-03-22", "555-5678"],
+        [3, "Dr. Carlos Ruiz", "Pediatria", "2019-11-15", "555-8765"]
+    ])
+
+@app.route("/registro_doctor")
+def registro_doctor():
+    return render_template("registor_doctor.html")
+
+@app.route("/editar_doctor/<int:id>")
+def editar_doctor(id):
+    usuario = [1, "Dr. Juan Perez", "Cardiologia", "2020-05-10", "555-1234"]
+    return render_template("editar_doctor.html", usuario=usuario)
 
 
 
